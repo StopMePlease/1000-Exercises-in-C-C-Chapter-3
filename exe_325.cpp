e73e41cf31fe5bcb528e61d8eba76d13effcc911
Duc Do <dotrananhduc@gmail.com>
2016-08-08 14:52:31 +0700

    exe_325

---
+++ exe_325.cpp
@@ -0,0 +1,62 @@
+﻿/*
+	Exe_325: Tính tích các số chẵn trên 1 cột trong ma trận nguyên
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{	
+	for(int i = 0; i < n; i++)
+	{
+		int sum = 1;
+		for(int j = 0; j < m; j++)
+		{			
+			if(a[j][i] % 2 == 0)
+				sum *= a[j][i];			
+		}
+		cout << "Column " << i << ": " << sum << endl;
+	}
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+	
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);	
+	
+	return 0;
+}
