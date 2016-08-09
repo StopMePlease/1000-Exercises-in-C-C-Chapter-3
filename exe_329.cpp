d57e13d852dec3d62a5990f94e21de08eb31cec1
Duc Do <dotrananhduc@gmail.com>
2016-08-09 09:42:35 +0700

    exe_329

---
+++ exe_329.cpp
@@ -0,0 +1,63 @@
+﻿/*
+	Exe_329: Biến đổi ma trận bằng cách thay giá trị âm bằng trị tuyệt đối của nó
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{			
+	for(int i = 0; i < m; i++)
+	{		
+		for(int j = 0; j < n; j++)
+		{				
+			if(a[i][j] < 0)
+			{
+				a[i][j] *= -1;
+			}
+		}		
+	}	
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+	
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+	cout << "==================\n";
+	Output(a, m, n);
+	return 0;
+}
