d9709283333f1150b3d05db38843290a438cdb66
Duc Do <dotrananhduc@gmail.com>
2016-08-09 09:50:02 +0700

    exe_332

---
+++ exe_332.cpp
@@ -0,0 +1,64 @@
+﻿/*
+	Exe_332: Tính tổng các giá trị lẻ trên 1 cột của ma trận nguyên
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{			
+	for(int i = 0; i < n; i++)
+	{		
+		int sum = 0;
+		for(int j = 0; j < m; j++)
+		{				
+			if(a[j][i] % 2 != 0)
+			{
+				sum += a[j][i];
+			}
+		}	
+		cout << "Column " << i << ": " << sum << endl;
+	}	
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+	
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+	
+	return 0;
+}
