6a70b0ab493582c7c02568d127bfb7b0118ec03b
Duc Do <dotrananhduc@gmail.com>
2016-08-10 09:02:19 +0700

    exe_336

---
+++ exe_336.cpp
@@ -0,0 +1,69 @@
+﻿/*
+	Exe_336: Đếm tần suất xuất hiện của 1 giá trị x trong ma trận thực
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+int Result(int a[][100], int m, int n, int x)
+{			
+	int dem = 0;
+	for(int i = 0; i < m; i++)
+	{				
+		for(int j = 0; j < n; j++)
+		{				
+			if(a[i][j] == x)
+			{
+				dem++;
+			}
+		}			
+	}	
+	return dem;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	int x;
+	cout << "Input x: ";
+	cin >> x;
+	
+	Input(a, m, n);
+	Output(a, m, n);
+	int kq = Result(a, m, n, x);
+	cout << kq;
+	
+	return 0;
+}
