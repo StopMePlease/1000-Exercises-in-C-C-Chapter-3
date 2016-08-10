5168ca42d0311865c286d4b34f24efa694ac378e
Duc Do <dotrananhduc@gmail.com>
2016-08-10 09:16:22 +0700

    exe_341

---
+++ exe_341.cpp
@@ -0,0 +1,68 @@
+﻿/*
+	Exe_341: Đếm số lượng số dương trên biên ma trận thực
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+int Result(int a[][100], int m, int n)
+{				
+	int dem = 0;
+	for(int i = 0; i < m; i++)
+	{					
+		for(int j = 0; j < n; j++)
+		{				
+			if(i == 0 || i == m - 1 || j == 0 || j == n - 1)
+			{
+				if(a[i][j] > 0)
+				{
+					dem++;
+				}
+			}
+		}	
+	}	
+	return dem;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	int kq = Result(a, m, n);
+	cout << kq;
+	
+	return 0;
+}
