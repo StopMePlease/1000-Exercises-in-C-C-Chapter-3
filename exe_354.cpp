9c6fd206716c27fdb7d921ea09ec1e0c4b76562e
Duc Do <dotrananhduc@gmail.com>
2016-08-13 11:36:11 +0700

    exe_354

---
+++ exe_354.cpp
@@ -0,0 +1,76 @@
+﻿/*
+	Exe_354: Kiểm tra trong ma trận có giảm dần theo hàng và theo cột hay không
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+int Result(int a[][100], int m, int n)
+{			
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			if(a[j][i] < a[j + 1][i])
+			{
+				return 0;
+			}
+		}
+	}
+
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] < a[i][j + 1])
+			{
+				return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	int kq = Result(a, m, n);
+	cout << kq;
+	
+	return 0;
+}
