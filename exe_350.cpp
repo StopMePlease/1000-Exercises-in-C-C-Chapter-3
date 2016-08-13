d76fcb4b09510278a6a6591d8a492efb11fca5b3
Duc Do <dotrananhduc@gmail.com>
2016-08-13 11:26:00 +0700

    exe_350

---
+++ exe_350.cpp
@@ -0,0 +1,64 @@
+﻿/*
+	Exe_350: Kiểm tra ma trận có tồn tại số lẻ hay không
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+int Result(int a[][100], int m, int n)
+{			
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] % 2 != 0)
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	int kq = Result(a, m, n);
+	cout << kq;
+	
+	return 0;
+}
