5a1a27be239bdb6e382c4d86f3c86dc5f52851a3
Duc Do <dotrananhduc@gmail.com>
2016-08-13 11:21:56 +0700

    exe_349

---
+++ exe_349.cpp
@@ -0,0 +1,77 @@
+﻿/*
+	Exe_349: Kiểm tra ma trận có tồn tại số hoàn thiện hay không
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+bool Check(int n)
+{
+	int sum = 0;
+	for(int i = 1; i < n; i++)
+	{
+		if(n % i == 0)
+		{
+			sum += i;
+		}
+	}
+	return sum == n ? true : false;
+}
+
+int Result(int a[][100], int m, int n)
+{			
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(Check(a[i][j]))
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	int kq = Result(a, m, n);
+	cout << kq;
+	
+	return 0;
+}
