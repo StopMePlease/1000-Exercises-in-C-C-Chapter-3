9f8aedc57224b79f2156dcbc3f8746909044bcdd
Duc Do <dotrananhduc@gmail.com>
2016-08-15 08:50:57 +0700

    exe_363

---
+++ exe_363.cpp
@@ -0,0 +1,73 @@
+﻿/*
+	Exe_363: Liệt kê các cột tăng dần trong ma trận thực
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{			
+	for(int i = 0; i < n; i++)
+	{
+		int check = 0;
+		for(int j = 0; j < m; j++)
+		{
+			if(a[j][i] < a[j + 1][i])
+			{
+				check = 1;
+				break;
+			}
+		}
+		if(check == 0)
+		{
+			cout << "Clumn " << i << " : ";
+			for(int j = 0 ; j < n; j++)
+			{
+				cout << setw(5) << a[j][i];
+			}
+			cout << endl;
+		}
+	}
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+	
+	return 0;
+}
