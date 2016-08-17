172e34c3150bcdb4b65cd13f8dc20fdb19565374
Duc Do <dotrananhduc@gmail.com>
2016-08-18 06:44:15 +0700

    exe_368

---
+++ exe_368.cpp
@@ -0,0 +1,81 @@
+﻿/*
+	Exe_368: Tìm giá trị lớn thứ nhì trong ma trận.
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+int Result(int a[][100], int m, int n)
+{
+	int max = a[0][0];
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] > max)
+			{
+				max = a[i][j];
+			}
+		}
+	}
+	
+	int max2 = a[0][0];
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] == max)
+			{
+				continue;
+			}
+			else if(a[i][j] > max2)
+			{
+				max2 = a[i][j];
+			}
+		}
+	}
+	return max2;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	int kq = Result(a, m, n);
+	cout << kq;
+
+	return 0;
+}
