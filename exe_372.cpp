a8c3d09e2277fb3ae3a6133978c94892215bac23
Duc Do <dotrananhduc@gmail.com>
2016-08-18 06:55:07 +0700

    exe_372

---
+++ exe_372.cpp
@@ -0,0 +1,66 @@
+﻿/*
+	Exe_372: Tìm giá trị lớn nhất trên 1 dòng trong ma trận thực
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{
+	int max = a[0][0];
+	
+	for(int i = 0; i < m; i++)
+	{
+		int max = a[i][0];
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] > max)
+			{
+				max = a[i][j];
+			}
+		}
+		cout << "Row " << i << " : " << max << endl;
+	}
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+
+	return 0;
+}
