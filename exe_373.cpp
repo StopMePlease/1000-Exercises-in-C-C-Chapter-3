6c95c7516645068b87d20de5956202e8de20ca5e
Duc Do <dotrananhduc@gmail.com>
2016-08-19 14:41:17 +0700

    exe_373

---
+++ exe_373.cpp
@@ -0,0 +1,64 @@
+﻿/*
+	Exe_373: Tìm giá trị nhỏ nhất trên 1 cột trong ma trận thực
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{	
+	for(int i = 0; i < n; i++)
+	{
+		int max = a[0][i];
+		for(int j = 0; j < m; j++)
+		{
+			if(a[j][i] > max)
+			{
+				max = a[j][i];
+			}
+		}
+		cout << "Column " << i << " : " << max << endl;
+	}
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+
+	return 0;
+}
