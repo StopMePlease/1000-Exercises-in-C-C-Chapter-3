3644b0f5bafaebe072bd502190986f3afe0bb67f
Duc Do <dotrananhduc@gmail.com>
2016-08-23 07:33:01 +0700

    exe_380

---
+++ exe_380.cpp
@@ -0,0 +1,98 @@
+﻿/*
+	Exe_380: Đếm số lượng giá trị chẵn nhỏ nhất trong ma trận thực
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{	
+	int count_even = -1;
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] % 2 == 0)
+			{
+				count_even = a[i][j];
+				break;
+			}
+		}
+		if(count_even % 2 == 0)
+		{
+			break;
+		}
+	}
+
+	if(count_even == -1)
+	{
+		cout << 0;
+		return ;
+	}
+
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] % 2 == 0 && a[i][j] < count_even)
+			{
+				count_even = a[i][j];
+			}
+		}
+	}
+
+	int dem = 0;
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(a[i][j] == count_even)
+			{
+				dem++;
+			}
+		}
+	}
+	cout << endl << dem;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+
+	return 0;
+}
