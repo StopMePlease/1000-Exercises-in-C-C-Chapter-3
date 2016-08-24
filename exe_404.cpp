bb5c7f6f3e560a10d0ddf4eb47ee05d68835beb8
Duc Do <dotrananhduc@gmail.com>
2016-08-25 06:58:27 +0700

    exe_404

---
+++ exe_404.cpp
@@ -0,0 +1,60 @@
+﻿/*
+	Exe_404: Xoay ma trận 1 góc 270`
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{	
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			cout << setw(5) << a[j][i];
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+
+	return 0;
+}
