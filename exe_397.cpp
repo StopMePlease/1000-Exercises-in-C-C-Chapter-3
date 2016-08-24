229cbcd1f83e402e3b92b1d21ce9039879bc00bd
Duc Do <dotrananhduc@gmail.com>
2016-08-24 11:38:49 +0700

    exe_397

---
+++ exe_397.cpp
@@ -0,0 +1,71 @@
+﻿/*
+	Exe_397: Dịch phải xoay vòng các cột trong ma trận
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int m, int n)
+{	
+	int temp[100];
+	for(int i = 0; i < n; i++)
+	{
+		temp[i] = a[i][n - 1];
+	}
+	
+	for(int i = n - 1; i > 0; i--)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			a[j][i] = a[j][i - 1];
+		}
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		a[i][0] = temp[i];
+	}
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+	Output(a, m, n);
+
+	return 0;
+}
