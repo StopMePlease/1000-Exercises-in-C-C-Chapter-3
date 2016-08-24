a5ed339a7e41e4b262d30dab5cfa2cd193cc70f9
Duc Do <dotrananhduc@gmail.com>
2016-08-24 22:10:46 +0700

    exe_400

---
+++ exe_400.cpp
@@ -0,0 +1,69 @@
+﻿/*
+	Exe_400: Xóa 1 dòng trong ma trận
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+void Result(int a[][100], int &m, int n)
+{	
+	int r;
+	cout << "delete row :";
+	cin >> r;
+
+	if(r == m - 1)
+	{
+		m--;
+		return;
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		a[r][i] = a[m - 1][i];
+	}
+	m--;
+}
+
+int main()
+{
+	int a[100][100];
+	int n, m;
+	cout << "Input rows: ";
+	cin >> m;
+	cout << "Input columns: ";
+	cin >> n;
+
+	Input(a, m, n);
+	Output(a, m, n);
+	Result(a, m, n);
+	cout << endl;
+	Output(a, m, n);
+
+	return 0;
+}
